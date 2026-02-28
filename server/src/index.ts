ea26a432d018305548a5a292bc4e3e1f76dff96a
Yuto Nagata <yuto7687@gmail.com>
2026-02-28 16:58:07 +0900

    Expressで簡単なAPIを追加

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+const PORT = 8080;
+
+app.get("/allTodo", (req: Request, res: Response) => {
+  return res.send("Todos");
+});
+
+app.listen(PORT, () => console.log("server is running"));
