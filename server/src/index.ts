972c9fcb373857d47044e72f9dd06d47f9039fb3
Yuto Nagata <yuto7687@gmail.com>
2026-03-01 14:47:46 +0900

    全てのTodoを取得するAPIを作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import { prisma } from "./lib/prisma";
 
 const app: Express = express();
 const PORT = 8080;
 
-app.get("/allTodo", (req: Request, res: Response) => {
-  return res.send("Todos");
+const allTodos = async () => {
+  const todos = await prisma.todo.findMany();
+  return todos;
+};
+app.get("/allTodo", async (req: Request, res: Response) => {
+  const todos = await allTodos();
+  return res.json(todos);
 });
 
 app.listen(PORT, () => console.log("server is running"));
