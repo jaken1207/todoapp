9cc5ac84843be3adc0c50e6e75c73b71d9a1b445
Yuto Nagata <yuto7687@gmail.com>
2026-03-01 18:03:54 +0900

    Todoを作成するAPIエンドポイントを作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { prisma } from "./lib/prisma";
 const app: Express = express();
 const PORT = 8080;
 
+app.use(express.json());
 const allTodos = async () => {
   const todos = await prisma.todo.findMany();
   return todos;
@@ -12,6 +13,19 @@ const allTodos = async () => {
 app.get("/allTodo", async (req: Request, res: Response) => {
   const todos = await allTodos();
   return res.json(todos);
+});
+app.post("/createTodo", async (req: Request, res: Response) => {
+  const { title, isCompleted } = req.body as {
+    title: string;
+    isCompleted?: boolean;
+  };
+  const createTodos = await prisma.todo.create({
+    data: {
+      title: title,
+      isCompleted: Boolean(isCompleted),
+    },
+  });
+  return res.json(createTodos);
 });
 
 app.listen(PORT, () => console.log("server is running"));
