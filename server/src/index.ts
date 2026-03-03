f0f0909dc4d09310d8564111e83a58d6d54c4740
Yuto Nagata <yuto7687@gmail.com>
2026-03-03 23:38:47 +0900

    apprunnerのportの宣言の仕方を修正

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import { prisma } from "./lib/prisma";
 import cors from "cors";
 
 const app: Express = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
